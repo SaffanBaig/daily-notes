321308ffd18fe2d50ca529c424a9ff476497f96b
SaffanBaig <mirzasaffan492@gmail.com>
2024-06-06 15:46:08 +0500

    Added postinstall

--- app/_components/notes/NotesList.tsx
+++ app/_components/notes/NotesList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { prisma } from "@/app/lib/prisma";
 import { getCurrentUser } from "@/app/lib/session";
 import Item from "./item";
