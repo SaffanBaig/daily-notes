cde3e54be60a221957d6d12336f46dae7834231a
SaffanBaig <mirzasaffan492@gmail.com>
2024-08-03 20:52:04 +0500

    Added email in jwt claims

--- app/lib/session.ts
+++ app/lib/session.ts
@@ -2,12 +2,20 @@ import { auth } from "@clerk/nextjs/server"
 import prisma from "./prisma";
 
 export const getCurrentUser = async () => {
-    const {userId} = auth();
+    const {userId, sessionClaims } = auth();
     if (!userId) throw new Error('Unauthorized');
-    const currentUser = await prisma.user.findUnique({
+    let currentUser = await prisma.user.findUnique({
         where: {
             clerkId: userId
         }
     })
+    if (!currentUser && sessionClaims?.email) {
+        currentUser = await prisma.user.create({
+          data: {
+            email:  sessionClaims.email.toString(),
+            clerkId: userId,
+          },
+        });
+      }
     return currentUser
 }
